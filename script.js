5284d4ac749d4a0b948638461875ce54e35cb736
Chris Mayes <coachchristac@gmail.com>
2019-03-06 17:50:32 -0500

    Finished styling header, added rotating banner image
    -Banner rotates through 3 images at 10s intervals.
    
    Images 1 & 2 require additional styling.

--- script.js
+++ script.js
@@ -1,18 +1,60 @@
-const menu = document.getElementById('menu');
-const toggleView = document.getElementById('toggleView');
-
-function addActive(){
-    toggleView.className += " active";
-    this.className += " active";
-    this.removeEventListener("click", addActive);
-    this.addEventListener("click", removeActive);
+// Declare elements
+const els = {
+    menu: document.getElementById('menu'),
+    toggleView: document.getElementById('toggleView'),
+    bannerImage: document.getElementById('bannerImage')
 }
 
-function removeActive(){
-    toggleView.className = "";
-    this.className = this.className.replace(/ active/, "");
-    this.removeEventListener("click", removeActive);
-    this.addEventListener("click", addActive);
+//Setup and implement expanding menu button (mobile view only)
+const clickHandlers = {
+    addActive: function(){
+        els.toggleView.className += " active";
+        this.className += " active";
+        this.removeEventListener("click", clickHandlers.addActive);
+        this.addEventListener("click", clickHandlers.removeActive);
+    },
+    removeActive: function(){
+        els.toggleView.className = "";
+        this.className = this.className.replace(/ active/, "");
+        this.removeEventListener("click", clickHandlers.removeActive);
+        this.addEventListener("click", clickHandlers.addActive);
+    }
 }
 
-menu.addEventListener("click", addActive);
+els.menu.addEventListener("click", clickHandlers.addActive);
+
+// Setup & implement rotating banner image
+function Counter(cb){
+    let i = 0;
+    this.getI = () => i;
+    this.increment = () => i++;
+    this.reset = () => {
+        i = 0;
+    }
+    this.cb = cb;
+}
+
+const imageCycler = new Counter(function(arr){
+    const i = this.getI();
+    if(i < arr.length-1) this.increment()
+    else this.reset();
+    return function(){
+        els.bannerImage.className = els.bannerImage.className.replace(/\d/, (i+''));
+    }
+})
+
+const timers = {
+    imageTimer: function(){
+        const timerPromise = new Promise(function(resolve, err){
+            setTimeout(function() {
+                resolve(imageCycler.cb([0,1,2]));
+              }, 5000)
+            }).then(function(cycle){
+                cycle();
+                timers.imageTimer();
+            })
+            .catch(() => console.log('imageTimer error'));
+        return timerPromise;
+    }
+}
+timers.imageTimer();
